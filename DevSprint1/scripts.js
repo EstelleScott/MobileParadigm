df7d5b32a342069aec748c01e2243980fbd20ef0
estelles <estiealysse@hotmail.com>
2020-10-08 22:03:09 +0930

    Wrote JS to show and hide the 'add from' container when you press the + button

--- DevSprint1/scripts.js
+++ DevSprint1/scripts.js
@@ -11,3 +11,14 @@ function showVideo(media) {
   var video = document.querySelector('video');
   video.srcObject = media;
 }
+
+// code to open the 'add pic from' div
+function openAddFrom(){
+  var container = document.getElementById("addFromContainer");
+  container.style.display = "block";
+}
+//code to close the 'add pic from' div
+function closeAddFrom(){
+  var container = document.getElementById("addFromContainer");
+  container.style.display = "none";
+}
